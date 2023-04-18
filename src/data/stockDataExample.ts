1e3dbf6c95ec086793f90b7cf7d60cc06e3e9a65
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-04-18 17:48:30 -0500

    add example data

---
+++ src/data/stockDataExample.ts
@@ -0,0 +1,25 @@
+export const stockData = [
+  {
+    ticker: "AAPL",
+    name: "Apple",
+    exchange_short: "NASDAQ",
+    price: 164.9,
+    currency: "USD",
+    day_change: 2.9,
+  },
+  {
+    ticker: "GOOG",
+    name: "Google",
+    exchange_short: "NASDAQ",
+    price: 107.15,
+    currency: "USD",
+    day_change: 2.5,
+  },
+  {
+    ticker: "AMZN",
+    name: "Amazon",
+    exchange_short: "NASDAQ",
+    price: 186.08,
+    day_change: 2.99,
+  },
+];
