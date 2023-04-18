99cedbf7840480dad01ad42747a13b4964ccba06
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-04-18 17:41:09 -0500

    add value, form event listeners, set footer at the bottom of the page

--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { SyntheticEvent } from "react";
 
 type TradeProps = {
   type: string;
 };
 
 const Trade = (tradeProps: TradeProps) => {
+  const [tradeFormData, setTradeFormData] = useState({
+    asset: "",
+    action: "",
+    quantity: "",
+    orderType: "",
+    duration: ""
+  });
+
+  function handleChange(event: SyntheticEvent){
+    const { name, value} = event.target as HTMLInputElement;
+    return setTradeFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    })
+
+  }
+
+  function handleSubmit(event: SyntheticEvent){
+    event.preventDefault();
+    // TODO: submit form data to database
+    console.log("submit form");
+  }
+  
   return (
-    <article className="rounded-lg p-6 w-full sm:w-4/12 border border-neutral-400 dark:bg-neutral-800 dark:border-0">
-      <form>
+    <article className="rounded-lg p-6 w-full lg:w-4/12 border border-neutral-400 dark:bg-neutral-800 dark:border-0">
+      <form onSubmit={handleSubmit}>
         <h2 className="text-xl font-semibold">Trade</h2>
         {tradeProps.type === "crypto" ? (
           <div className="mt-6">
             <label className="block" htmlFor="asset">Symbol</label>
-            <select className="rounded-lg w-full p-2" name="asset">
+            <select className="rounded-lg w-full p-2" name="asset" value={tradeFormData.asset} onChange={handleChange}>
               <option value="BTC">Bitcoin</option>
               <option value="ETH">Ethereum</option>
             </select>
@@ -20,7 +46,7 @@ const Trade = (tradeProps: TradeProps) => {
         ) : (
           <div className="mt-6">
             <label className="block" htmlFor="asset">Symbol</label>
-            <select className="rounded-lg w-full p-2" name="asset">
+            <select className="rounded-lg w-full p-2" name="asset" value={tradeFormData.asset} onChange={handleChange}>
               <option value="AAPL">AAPL</option>
               <option value="TSLA">TSLA</option>
               <option value="GOOG">GOOG</option>
@@ -29,24 +55,30 @@ const Trade = (tradeProps: TradeProps) => {
         )}
         <div className="mt-6">
           <label className="block" htmlFor="action">Action</label>
-          <select className="rounded-lg w-full p-2" name="action">
+          <select className="rounded-lg w-full p-2" name="action" value={tradeFormData.action} onChange={handleChange}>
             <option value="Buy">Buy</option>
             <option value="Sell">Sell</option>
           </select>
         </div>
         <div className="mt-6">
           <label className="block" htmlFor="quantity">Quantity</label>
-          <input className="rounded-lg w-full p-2" type="number" name="quantity" />
+          <input className="rounded-lg w-full p-2" type="number" name="quantity" value={tradeFormData.quantity} onChange={handleChange} />
         </div>
         <div className="mt-6">
           <label className="block" htmlFor="orderType">Order Type</label>
-          <input className="rounded-lg w-full p-2" type="text" name="orderType" />
+          <select className="rounded-lg w-full p-2" name="orderType" value={tradeFormData.orderType} onChange={handleChange}>
+            <option value="Market">Market</option>
+            <option value="Limit">Limit</option>
+          </select>
         </div>
         <div className="mt-6">
           <label className="block" htmlFor="duration">Duration</label>
-          <input className="rounded-lg w-full p-2" type="text" name="duration" />
+          <select className="rounded-lg w-full p-2" name="duration" value={tradeFormData.duration} onChange={handleChange}>
+            <option value="Day">Day</option>
+            <option value="Until Cancelled">Until Cancelled</option>
+          </select>
         </div>
-        <button className="rounded-lg bg-blue-600 dark:bg-blue-400 text-neutral-50 py-2 px-4 mt-6 w-full">Submit</button>
+        <button type="submit" className="rounded-lg bg-blue-600 dark:bg-blue-400 text-neutral-50 py-2 px-4 mt-6 w-full">Submit</button>
       </form>
     </article>
   );
