2d7489c1a0c960927c77700885103f5b8f0e6f92
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-04-29 15:35:01 -0500

    add portfolio state and add as prop to child components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,40 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { ReactNode } from "react";
+import React, { ReactElement, ReactNode, useState } from "react";
+import { portfolioData } from "@/data/portfolioDataExample";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const [portfolio, setPortfolio] = useState(portfolioData);
+
+  function updatePortfolio(tradeTotal: number, action: string) {
+    return setPortfolio((prevPortfolio) => {
+      const cashTotal =
+        action.toUpperCase() === "BUY"
+          ? prevPortfolio.cash - tradeTotal
+          : prevPortfolio.cash + tradeTotal;
+      return {
+        ...prevPortfolio,
+        cash: cashTotal,
+      };
+    });
+  }
+
   return (
     <div className="flex flex-col items-stretch min-h-full">
       <Navbar />
-      <main className="p-6 grow shrink-0">{children}</main>
+      <main className="p-6 grow shrink-0">
+        {React.Children.map(children, (child) => {
+          return React.cloneElement(child as ReactElement, {
+            portfolio,
+            updatePortfolio,
+          });
+        })}
+      </main>
+
       <Footer />
     </div>
   );
