7b8aa192241bbe0d50d070b85737c736466f1a92
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-04-29 15:35:46 -0500

    add links to nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,14 @@ const Navbar = () => {
   return (
     <header className="p-4 border-b border-neutral-600 dark:border-neutral-400 shrink-0">
       <nav className="flex">
-        <div className="mr-auto text-lg">Trade To Learn</div>
+        <div className="mr-auto text-lg">
+          <Link href="/">Trade To Learn</Link>
+        </div>
         <ul className="flex gap-4">
-          <li>Crypto</li>
-          <li>Portfolio</li>
-          <li>Log In</li>
+          <li><Link href="/portfolio">Portfolio</Link></li>
+          <li><Link href="/">Stocks</Link></li>
+          <li><Link href="/crypto">Crypto</Link></li>
+          {/* <li>Log In</li> */}
         </ul>
       </nav>
     </header>
