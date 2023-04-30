c1d1725bbcb468730f40e5ff342bc3ced50c6fa7
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-04-30 10:18:55 -0500

    update form submission event handler to update transactions and portfolio

--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -1,84 +1,150 @@
 import { useState, useEffect } from "react";
 import { SyntheticEvent } from "react";
+import { transactions } from "@/data/transactionsExample";
+import TradeRequest from "@/interfaces/TradeRequest";
+import TradeQuoteData from "@/interfaces/TradeQuoteData";
 
 type TradeProps = {
-  type: string;
+  updatePortfolio: (tradeTotal: number, action: string) => void;
+  tradeQuoteData: TradeQuoteData[];
 };
 
-const Trade = (tradeProps: TradeProps) => {
+const Trade: React.FC<TradeProps> = ({ tradeQuoteData, updatePortfolio }) => {
   const [tradeFormData, setTradeFormData] = useState({
-    asset: "",
+    ticker: "",
     action: "",
-    quantity: "",
+    quantity: 0,
     orderType: "",
-    duration: ""
+    duration: "",
   });
 
-  function handleChange(event: SyntheticEvent){
-    const { name, value} = event.target as HTMLInputElement;
+  function handleChange(event: SyntheticEvent) {
+    const { name, value } = event.target as HTMLInputElement;
     return setTradeFormData((prevFormData) => {
       return {
         ...prevFormData,
-        [name]: value
-      }
-    })
-
+        [name]: value,
+      };
+    });
   }
 
-  function handleSubmit(event: SyntheticEvent){
+  function handleSubmit(event: SyntheticEvent) {
     event.preventDefault();
+    const tradeQuote = getTradeDetails(tradeFormData.ticker);
+    const name = tradeQuote?.name;
+    const price = tradeQuote?.price ? tradeQuote.price : 0;
+    // TODO: If action is to sell, verify the user owns that amount of select assets
     // TODO: submit form data to database
-    console.log("submit form");
+    transactions.push({
+      ...tradeFormData,
+      name,
+      total: tradeFormData.quantity * price,
+      date: new Date().toISOString(),
+      price,
+    } as TradeRequest);
+    // update portfolio
+    updatePortfolio(tradeFormData.quantity * price, tradeFormData.action);
   }
-  
+
+  function getTradeDetails(ticker: string) {
+    for (const quoteData of tradeQuoteData) {
+      if (quoteData.ticker === ticker) {
+        return quoteData;
+      }
+    }
+  }
+
+  function getUserHoldings() {}
+
   return (
     <article className="rounded-lg p-6 w-full lg:w-4/12 border border-neutral-400 dark:bg-neutral-800 dark:border-0">
       <form onSubmit={handleSubmit}>
-        <h2 className="text-xl font-semibold">Trade</h2>
-        {tradeProps.type === "crypto" ? (
-          <div className="mt-6">
-            <label className="block" htmlFor="asset">Symbol</label>
-            <select className="rounded-lg w-full p-2" name="asset" value={tradeFormData.asset} onChange={handleChange}>
-              <option value="BTC">Bitcoin</option>
-              <option value="ETH">Ethereum</option>
-            </select>
-          </div>
-        ) : (
-          <div className="mt-6">
-            <label className="block" htmlFor="asset">Symbol</label>
-            <select className="rounded-lg w-full p-2" name="asset" value={tradeFormData.asset} onChange={handleChange}>
-              <option value="AAPL">AAPL</option>
-              <option value="TSLA">TSLA</option>
-              <option value="GOOG">GOOG</option>
-            </select>
-          </div>
-        )}
+        <h2 className="text-lg">Trade</h2>
         <div className="mt-6">
-          <label className="block" htmlFor="action">Action</label>
-          <select className="rounded-lg w-full p-2" name="action" value={tradeFormData.action} onChange={handleChange}>
+          <label className="block" htmlFor="ticker">
+            Symbol
+          </label>
+          <select
+            className="rounded-lg w-full p-2"
+            name="ticker"
+            value={tradeFormData.ticker}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Please select an asset</option>
+            {tradeQuoteData.map((data, index) => (
+              <option value={data.ticker} key={index}>
+                {data.ticker} - {data.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mt-6">
+          <label className="block" htmlFor="action">
+            Action
+          </label>
+          <select
+            className="rounded-lg w-full p-2"
+            name="action"
+            value={tradeFormData.action}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Please choose an action</option>
             <option value="Buy">Buy</option>
             <option value="Sell">Sell</option>
           </select>
         </div>
         <div className="mt-6">
-          <label className="block" htmlFor="quantity">Quantity</label>
-          <input className="rounded-lg w-full p-2" type="number" name="quantity" value={tradeFormData.quantity} onChange={handleChange} />
+          <label className="block" htmlFor="quantity">
+            Quantity
+          </label>
+          <input
+            className="rounded-lg w-full p-2"
+            type="number"
+            name="quantity"
+            value={tradeFormData.quantity}
+            onChange={handleChange}
+            required
+            min={0}
+          />
         </div>
         <div className="mt-6">
-          <label className="block" htmlFor="orderType">Order Type</label>
-          <select className="rounded-lg w-full p-2" name="orderType" value={tradeFormData.orderType} onChange={handleChange}>
+          <label className="block" htmlFor="orderType">
+            Order Type
+          </label>
+          <select
+            className="rounded-lg w-full p-2"
+            name="orderType"
+            value={tradeFormData.orderType}
+            onChange={handleChange}
+            required
+          >
             <option value="Market">Market</option>
             <option value="Limit">Limit</option>
           </select>
         </div>
         <div className="mt-6">
-          <label className="block" htmlFor="duration">Duration</label>
-          <select className="rounded-lg w-full p-2" name="duration" value={tradeFormData.duration} onChange={handleChange}>
+          <label className="block" htmlFor="duration">
+            Duration
+          </label>
+          <select
+            className="rounded-lg w-full p-2"
+            name="duration"
+            value={tradeFormData.duration}
+            onChange={handleChange}
+            required
+          >
             <option value="Day">Day</option>
             <option value="Until Cancelled">Until Cancelled</option>
           </select>
         </div>
-        <button type="submit" className="rounded-lg bg-blue-600 dark:bg-blue-400 text-neutral-50 py-2 px-4 mt-6 w-full">Submit</button>
+        <button
+          type="submit"
+          className="rounded-lg bg-blue-600 dark:bg-blue-400 text-neutral-50 py-2 px-4 mt-6 w-full"
+        >
+          Submit
+        </button>
       </form>
     </article>
   );
