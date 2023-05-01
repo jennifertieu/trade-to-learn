9362851ab0a3beee3f98031c3a46d68ba32b38f0
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-01 10:17:04 -0500

    remove heading

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -3,8 +3,7 @@ import PortfolioProps from "@/interfaces/PortfolioProps";
 const PortfolioCard = ({ portfolio, updatePortfolio }: PortfolioProps) => {
   return (
     <article className="p-4 rounded-lg border border-neutral-400 dark:bg-neutral-800 dark:border-0">
-      <h2 className="text-lg">Portfolio</h2>
-      <div className="flex mt-2">
+      <div className="flex">
         <div>
           <div className="text-sm text-neutral-700 dark:text-neutral-300">
             Cash
