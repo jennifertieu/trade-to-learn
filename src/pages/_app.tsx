f3d767eeabe4964728e302ba4abce7664b7f2f12
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-03 16:14:29 -0500

    add react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <ToastContainer />
     </Layout>
   );
 }
