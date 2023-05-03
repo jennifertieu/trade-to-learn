c5f66cf1e5f9720ddeda26ce7f3bf9b7b314e528
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-03 16:15:52 -0500

    update portfolio props

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -1,6 +1,11 @@
-import PortfolioProps from "@/interfaces/PortfolioProps";
+import PortfolioProps from "@/types/PortfolioProps";
 
-const PortfolioCard = ({ portfolio, updatePortfolio }: PortfolioProps) => {
+const PortfolioCard = ({
+  portfolio,
+  updateCash,
+  updateUserHoldings,
+  getUserHoldings,
+}: PortfolioProps) => {
   return (
     <article className="p-4 rounded-lg border border-neutral-400 dark:bg-neutral-800 dark:border-0">
       <div className="flex">
