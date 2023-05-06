e28b86098b1cc02e4e111c180c8a90ec0d44a67d
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-06 14:48:28 -0500

    remove example transaction data

--- src/data/transactionsExample.ts
+++ src/data/transactionsExample.ts
@@ -1,15 +1,3 @@
 import TradeRequest from "@/interfaces/TradeRequest";
 
-export const transactions: TradeRequest[] = [
-    // {
-    //     ticker: "ETH",
-    //     name: "Ethereum",
-    //     date: "1/1/2023",
-    //     price: 1869.83,
-    //     quantity: .10696,
-    //     total: 223.74,
-    //     orderType: "Market",
-    //     duration: "24",
-    //     action: "Buy"
-    // }    
-];
+export const transactions: TradeRequest[] = [];
