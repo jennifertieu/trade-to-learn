fb87cdebf58b8db4d2bd7193d358e055825eab0e
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-06 14:50:33 -0500

    remove getUserHoldings function, function was migrated to usePortfolio hook

--- src/lib/validateTrade.ts
+++ src/lib/validateTrade.ts
@@ -1,4 +1,3 @@
-import Portfolio from "@/interfaces/Portfolio";
 import TradeQuoteData from "@/interfaces/TradeQuoteData";
 
 export function getQuoteDetails(
@@ -10,17 +9,4 @@ export function getQuoteDetails(
       return quoteData;
     }
   }
-}
-
-export function getUserHoldings(
-  ticker: string,
-  quantity: number,
-  portfolio: Portfolio
-) {
-  for (const item of portfolio.stocks) {
-    if (item.ticker === ticker && item.quantity >= quantity) {
-      return true;
-    }
-  }
-  return false;
 }
