b8262f9dbc35f40a85c7f29bd17023a7eb6f35df
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-13 14:25:13 -0500

    delete user context file

--- src/context/UserContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext, ReactNode } from "react";
-
-export const UserContext = createContext(null);
-
-export function UserContextProvider(props: ReactNode) {
-  return <UserContext.Provider value={}>{props}</UserContext.Provider>;
-}
