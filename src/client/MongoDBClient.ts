0de533cfa3656900f0f1c88408601b58ecd34096
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-13 14:20:36 -0500

    rename client variable to mongoClientPromise

--- src/client/MongoDBClient.ts
+++ src/client/MongoDBClient.ts
@@ -8,24 +8,23 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
+let mongoClient;
+let mongoClientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  console.log("test");
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    mongoClient = new MongoClient(uri, options);
+    global._mongoClientPromise = mongoClient.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  mongoClientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  mongoClient = new MongoClient(uri, options);
+  mongoClientPromise = mongoClient.connect();
 }
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
-export default clientPromise;
+export default mongoClientPromise;
