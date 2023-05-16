ccdec067c8b9b75f9904b9f9db551f3a2303da17
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-15 21:24:57 -0500

    add authentication

--- src/pages/api/portfolio/[userId].ts
+++ src/pages/api/portfolio/[userId].ts
@@ -1,10 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import mongoClientPromise from "@/client/MongoDBClient";
+import { authOptions } from "../auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
+
   try {
     const db = (await mongoClientPromise).db();
     const portfolio = db.collection("portfolio");
@@ -20,9 +29,15 @@ export default async function handler(
         console.log(result);
         res.status(200).json(result);
         break;
+      case "PUT":
+        // update existing user portfolio data
+        break;
+      case "DELETE":
+        // delete user portfolio
+        break;
     }
   } catch (ex) {
     console.log(ex);
-    return ex;
+    throw ex;
   }
 }
