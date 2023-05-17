a0acbfa757c483f1d99994a39e9fa9543515982b
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-05-17 14:02:05 -0500

    add authorization

--- src/pages/api/stock-quote.ts
+++ src/pages/api/stock-quote.ts
@@ -1,10 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getStockQuote } from "@/client/StockDataClient";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
   try {
     const data = await getStockQuote();
     return res.status(200).json(data);
