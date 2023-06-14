464abe177de39805714bea30cdd15df5ec2e1601
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-14 16:31:19 -0500

    add conditional background color for home screen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,20 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import React, { ReactNode } from "react";
+import { useRouter } from "next/router";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter();
   return (
-    <div className="flex flex-col items-stretch h-full">
+    <div
+      className={`flex flex-col items-stretch h-full overflow-auto ${
+        router.pathname === "/" ? "bg-blue-600" : ""
+      }`}
+    >
       <Navbar />
       <main className="grow shrink-0 basis-auto">{children}</main>
       <Footer />
