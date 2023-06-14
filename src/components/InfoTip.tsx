ee31e2bdb4311616ed4876ec2b877b4578f54125
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-14 16:30:46 -0500

    refactor info box style to tailwind, add info descriptions, update styling

--- src/components/InfoTip.tsx
+++ src/components/InfoTip.tsx
@@ -1,17 +1,62 @@
-export default function InfoTip() {
-  // TODO: dynamically position tooltip over button
-  // calculate mouse position and window position
+type InfoTipProps = {
+  name: string;
+};
+
+export default function InfoTip({ name }: InfoTipProps) {
+  const tipDictionary: {
+    [key: string]: string;
+  } = {
+    ticker:
+      "A stock symbol or ticker is a unique series of letters assigned to a security for trading purpose",
+    "day change":
+      "For a stock or bond quote, day change is the difference between the current price and the last trade of the previous day",
+    "current price":
+      "Current price is the most recent price at which a security was sold on an exchange",
+    "purchase price":
+      "The price an investor pays for an investment, and the price becomes the investor’s cost basis for calculating gain or loss when selling the investment. To determine the cost basis of the purchases, the investor needs to calculate the weighted average cost, which is the total dollar amount of the purchases divided by the number of shares purchased.",
+    "order type":
+      "A market order is the most basic type of trade. It is an order to buy or sell immediately at the current price.",
+    action: "Buy/Sell allow you add/remove stocks from your portfolio.",
+    "total change":
+      "Subtract the total purchase price from the current price of the stock then divide that by the original purchase price and multiply that figure by 100. This gives you the total percentage change",
+    "total value":
+      "The current market price of the shares multiplied by the number of shares held for that specific stock (Market Value)",
+    cash: "Total amount of cash available for making trades.",
+    quote: "A stock quote is the price of a stock as quoted on an exchange",
+    holdings:
+      "Holdings are the contents of an investment portfolio held by an individual or an entity",
+  };
+  const tipDescription = tipDictionary[name.toLowerCase()];
   return (
     <>
-      <button className="info-button text-indigo-700 dark:text-indigo-300 ms-1">
-        i
-        <div className="info-box">
-          Both “stock market” and “stock exchange” are often used
-          interchangeably. Traders in the stock market buy or sell shares on one
-          or more of the stock exchanges that are part of the overall stock
-          market.
-        </div>
-      </button>
+      {tipDescription ? (
+        <button className="group inline-block relative border rounded-full h-4 w-4 text-center leading-3 text-sm text-blue-600 dark:text-blue-400 border-blue-600 dark:border-blue-400 ms-1">
+          i
+          <div className="group-hover:block hidden rounded w-72 p-4 md:p-2 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:absolute lg:top-0 lg:-translate-y-full text-black dark:text-white bg-blue-600 dark:bg-blue-400">
+            <div className="font-bold text-center mb-2 block lg:hidden">
+              {name}
+            </div>
+            <p className="mb-2 whitespace-normal leading-normal">
+              {tipDescription}
+            </p>
+            <small className="block md:hidden mb-2">
+              Definition sourced from{" "}
+              <a
+                className="underline"
+                href="https://www.investopedia.com/"
+                rel="noopener noreferrer"
+              >
+                Investopedia
+              </a>
+            </small>
+            <small className="block md:hidden text-gray-300">
+              Note: Click anywhere outside the window to close
+            </small>
+          </div>
+        </button>
+      ) : (
+        ""
+      )}
     </>
   );
 }
