34f28ca750ef601b1f662facb12e567172f5bf8b
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-19 17:10:19 -0500

    add default value

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,8 +21,8 @@ export const authOptions = {
       from: process.env.EMAIL_FROM,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
     // ...add more providers here
   ],
