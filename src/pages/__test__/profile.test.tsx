35c58ad8664135e7d9c0acbabe0538afb5492ab6
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-21 14:42:08 -0500

    add next-auth mock for session provider

--- src/pages/__test__/profile.test.tsx
+++ src/pages/__test__/profile.test.tsx
@@ -1,6 +1,26 @@
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import Profile from "../profile";
 
+jest.mock("next-auth/react", () => {
+  const originalModule = jest.requireActual("next-auth/react");
+  const mockSession = {
+    expires: new Date(Date.now() + 2 * 86400).toISOString(),
+    user: {
+      name: "Jennifer Tieu",
+      email: "test@email.com",
+      image: "",
+      id: "ABC123",
+    },
+  };
+  return {
+    __esModule: true,
+    ...originalModule,
+    useSession: jest.fn(() => {
+      return { data: mockSession, status: "authenticated" }; // return type is [] in v3 but changed to {} in v4
+    }),
+  };
+});
+
 describe("Profile Page", () => {
   it("render profile page", () => {
     render(<Profile />);
