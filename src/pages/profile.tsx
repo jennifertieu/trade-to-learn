7fab6376910f840d6ed02d6818271f7b5274c95c
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-21 14:29:07 -0500

    add loading animation

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import { MutableRefObject, useRef } from "react";
+import { MutableRefObject, useRef, useState } from "react";
 import { deleteUserPortfolio } from "@/lib/portfolioApiService";
 import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import { signOut } from "next-auth/react";
 
 export default function Profile() {
+  const [isExecuting, setIsExecuting] = useState(false);
   const { data: session } = useSession();
   const dialogRef: MutableRefObject<HTMLDialogElement | null> = useRef(null);
 
@@ -23,6 +24,7 @@ export default function Profile() {
     try {
       //close modal
       dialogRef.current?.close();
+      setIsExecuting(true);
       // delete user portfolio
       await deleteUserPortfolio(session);
       // delete user data
@@ -38,6 +40,8 @@ export default function Profile() {
       toast.error(
         "Something went wrong. Please contact support if application is not working."
       );
+
+      setIsExecuting(false);
     }
   }
 
@@ -50,10 +54,35 @@ export default function Profile() {
         <article className="text-center p-6 rounded-lg border border-neutral-400 dark:bg-neutral-800 dark:border-0">
           <h1 className="text-3xl">Profile</h1>
           <button
-            className="mt-4 py-2 px-4 rounded-lg bg-red-600 dark:bg-red-400"
+            className={`${
+              isExecuting ? "opacity-50" : "opacity-100"
+            } mt-4 py-2 px-10 rounded-lg bg-red-600 dark:bg-red-400 relative`}
             type="button"
             onClick={openModal}
+            disabled={isExecuting ? true : false}
           >
+            {isExecuting && (
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white absolute top-2.5 left-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            )}
             Delete Account
           </button>
         </article>
