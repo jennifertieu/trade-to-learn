4503e2a2e5d2bfaa200256943cb5de54e5757d92
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-22 16:41:27 -0500

    update about, update spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,11 @@ export default function Home() {
       <Head>
         <title>Trade To Learn | Welcome</title>
       </Head>
-      <section className="text-center py-8 flex flex-col justify-center items-center gap-2 bg-blue-600">
+      <section className="text-center py-8 flex flex-col justify-center items-center gap-6 bg-blue-600">
         <h1 className="climate-crisis-font text-5xl font-bold uppercase text-black">
           Welcome
         </h1>
-        <p className="text-black m-3">
-          A beginner friendly way to start trading
-        </p>
+        <p className="text-black">A beginner friendly way to start trading</p>
         <Link
           className="rounded-lg bg-violet-500 text-white py-2 px-4"
           href={session ? "/trade" : "/auth/signin"}
@@ -32,19 +30,24 @@ export default function Home() {
           className="px-2"
         />
       </section>
-      <section className="flex flex-col items-center p-4 text-black mt-10">
+      <section className="leading-normal flex flex-col items-center p-4 text-black mt-10">
         <h2 className="text-xl font-bold mt-3 text-white">About</h2>
-        <div className="flex gap-4 max-w-5xl flex-col md:flex-row">
-          <p className="climate-crisis-font mt-3 text-2xl md:w-1/2 p-4">
+        <div className="flex max-w-5xl flex-col justify-between md:flex-row">
+          <p className="climate-crisis-font mt-3 text-2xl p-4">
             An easy way to get started trading without the risk of losing real
             money.
           </p>
-          <p className="mt-3 p-4 md:w-1/2">
-            Trade to Learn is an educational application to help beginners to
-            learn about trading stocks using real-market data. Our application
-            is that it does not require any real money investment or the
-            submission of personal information during the sign-up process.
-          </p>
+          <div className="flex flex-col">
+            <p className="mt-3 p-4 text-xl">
+              Trade to Learn is a beginner-friendly way to start trading stocks.
+              It is a simple and easy-to-use trading application that uses
+              real-market data to help educate you on trading.
+            </p>
+            <p className="mt-3 p-4 text-xl">
+              Our application does NOT require any real money investment or the
+              submission of personal information during the sign-up process.
+            </p>
+          </div>
         </div>
       </section>
     </>
